438543cf1fa5d88c392a7d2e3b4a2d41101cafdb
Amr Safwat <amrsafwat23@hotmail,.com>
2025-07-06 22:30:54 +0300

    Files Modification: Creating shipping service

---
+++ Customer.java
@@ -0,0 +1,21 @@
+public class Customer {
+    private String name;
+    private double balance;
+
+    public Customer(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deduct(double amount) {
+        this.balance -= amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
