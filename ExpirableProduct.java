6cae5ff506dfe4ec5b62132408580b1f847f116f
Amr Safwat <amrsafwat23@hotmail,.com>
2025-07-06 21:53:03 +0300

    Files Modification: Added a new class for products

---
+++ ExpirableProduct.java
@@ -0,0 +1,22 @@
+import java.time.LocalDate;
+
+public class ExpirableProduct extends Product implements ShippableProduct {
+    private LocalDate expirationDate;
+    private double weight;
+
+    public ExpirableProduct(String name, double price, int quantity, LocalDate expiryDate, double weight) {
+        super(name, price, quantity);
+        this.expirationDate = expiryDate;
+        this.weight = weight;
+    }
+
+    @Override
+    public boolean isExpired() {
+        return LocalDate.now().isAfter(expirationDate);
+    }
+
+    @Override
+    public double getWeight() {
+        return weight;
+    }
+}
